3657c0e09fdce70eb67f82e119196dfdfecf236c
Najpraphatsarang <6005008245@rumail.ru.ac.th>
2025-06-12 12:43:28 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home.jsx";
-import Login from "./pages/login.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 import ProtectedRoute from "./components/ProtectedRoute"; // ปรับ path ตามที่คุณเก็บ
-import ForgotPassword from "./pages/forgotPasswordForm.jsx";
-import ResetPassword from "./pages/resetPassword.jsx"
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx"
 
 function App() {
   return (
