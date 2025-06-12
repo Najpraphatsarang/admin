f0e1a9577d3bb92a4bba2a6539915e43ea7d0e64
Najpraphatsarang <6005008245@rumail.ru.ac.th>
2025-06-12 12:03:18 +0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../src/pages/้home.jsx"; // หรือ ./components/Home
+import Home from "../src/pages/home.jsx"; // หรือ ./components/Home
 import Login from "../src/pages/login.jsx";
 import ProtectedRoute from "./components/ProtectedRoute"; // ปรับ path ตามที่คุณเก็บ
 import ForgotPassword from "./pages/forgotPasswordForm.jsx";
