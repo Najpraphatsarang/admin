c3c624e972141cb7660837428d3c760099ef8fb9
Najpraphatsarang <6005008245@rumail.ru.ac.th>
2025-06-12 12:26:18 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/้home.jsx";
+import Home from "./pages/home.jsx";
 import Login from "./pages/login.jsx";
 import ProtectedRoute from "./components/ProtectedRoute"; // ปรับ path ตามที่คุณเก็บ
 import ForgotPassword from "./pages/forgotPasswordForm.jsx";
-import ResetPassword from "./pages/resetPassword.jsx";
+import ResetPassword from "./pages/resetPassword.jsx"
 
 function App() {
   return (
